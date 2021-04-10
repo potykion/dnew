9c596cfb1c32d5518924d45f3ce5e303af6d76d3
potykion <potykion@gmail.com>
2021-04-10 22:30:58 +0300

    Show authorized user in settings

--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -1,5 +1,6 @@
 import 'package:dnew/logic/settings/display_mode/controllers.dart';
 import 'package:dnew/widgets/bottom.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:dnew/logic/settings/dark_mode/controllers.dart';
@@ -11,10 +12,17 @@ class SettingsPage extends HookWidget {
   Widget build(BuildContext context) {
     var displayModeStr = useProvider(displayModeStrProvider);
     var isDarkMode = useProvider(isDarkModeProvider);
+    var user = FirebaseAuth.instance.currentUser!;
 
     return Scaffold(
       body: ListView(
         children: [
+          ListTile(
+            leading:
+                CircleAvatar(backgroundImage: NetworkImage(user.photoURL!)),
+            title: Text(user.displayName!),
+            subtitle: Text("Подписка активна"),
+          ),
           ListTile(
             title: Text("Отображение записей"),
             trailing: Text(
